cd87b4f65b415db0b7272b771b3b8faa1c16950e
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2025-11-19 16:58:38 +0530

    Update index.ts

--- apps/mesh-wss/src/index.ts
+++ apps/mesh-wss/src/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const API_ENDPOINT = process.env.API_ENDPOINT!;
 
 const server = createServer((req, res) => {
-  if (req.url === "/health") {
+  if (req.url === "/health" || req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("OK");
     return;
