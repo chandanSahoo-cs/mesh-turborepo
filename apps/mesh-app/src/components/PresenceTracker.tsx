fea7d1e04678951e062c5888c44de7301257cb63
Chandan <chandansahoo02468@gmail.com>
2026-02-22 14:54:57 +0530

    next and react version update

--- apps/mesh-app/src/components/PresenceTracker.tsx
+++ apps/mesh-app/src/components/PresenceTracker.tsx
@@ -7,14 +7,14 @@ import 'dotenv/config'
 const PresenceTracker = () => {
   const { userData } = useCurrentUser();
 
-  const wssurl = process.env.NEXT_PUBLIC_WEBSOCKET_SERVER_URL;
+  const wssurl = process.env.NEXT_PUBLIC_WEBSOCKET_SERVER_URL || "";
   console.log("wssurl:", wssurl);
 
   useEffect(() => {
     if (!userData) return;
     const connect = async () => {
       const ws = new WebSocket(
-        `${wssurl}?userId=${userData?._id}`
+        `wss://mesh-turborepo.onrender.com?userId=${userData?._id}`
       );
       ws.onopen = () => console.log("Connected to [WSS]");
       ws.onmessage = (msg) => console.log("Message:", msg.data);
